1008713af23acaf097c6847d1d7931ba854d8f9e
Muggle Potter <sanjanatripathi007@gmail.com>
2018-09-06 13:18:39 -0500

    added controller code for each route - update, insert and create

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,11 +13,20 @@ router.get("/", function(req, res){
 });
 
 router.post("/api/burgers", function(req, res){
-
+    burger.insertOne(["burger_name"],[req.body.name], function(result){
+        res.json({id: result.insertId});
+    });
 });
 
 router.put("/api/burgers/:id", function(req, res){
-
+    burger.updateOne(devoured, req.body.devoured, id, req.params.id, function(result){
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+    });
 });
 
 module.exports = router;
