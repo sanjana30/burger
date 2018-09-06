ce41ae1bab0bcc38015db0448a4431d4dd09e813
Muggle Potter <sanjanatripathi007@gmail.com>
2018-09-06 16:25:29 -0500

    added code in index.handlebars to display entries from tables, passed column names and values in burgers_controller.js to fetch values from db and update/add to it, made some corrections in orm.js sql syntax

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,22 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
 var orm = {
     selectAll: function(tableName, cb){
-        var queryString = "SELECT * FROM ?";
+        var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableName], function(err, result){
             if(err) throw err;
             cb(result);
         });
     },
     insertOne: function(tableName, colName, burgerName, cb){
-        var queryString = "INSERT INTO ?(?) VALUES ?";
+        var queryString = "INSERT INTO ??(??) VALUES ?";
         connection.query(queryString, [tableName, colName, burgerName], function(err, result){
             if(err) throw err;
             cb(result);
         });
     },
     updateOne: function(tableName, devouredCol, devouredStatus, idCol, idValue, cb){
-        var queryString = "UPDATE ? SET ?=? WHERE ?=?";
+        var queryString = "UPDATE ?? SET ??=? WHERE ??=?";
         connection.query(queryString, [tableName, devouredCol, devouredStatus, idCol, idValue], function(err, result){
             if(err) throw err;
             cb(result);
